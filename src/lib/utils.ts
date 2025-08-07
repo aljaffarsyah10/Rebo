b4f73b7eb30403393b08fc250fcc60a5896a43da
aljaffarsyah <222011596@stis.ac.id>
2025-08-07 14:45:18 +0800

    add conf supabase env

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -24,3 +24,8 @@ export function formatBytes(
       : (sizes[i] ?? 'Bytes')
   }`;
 }
+
+// This check can be removed, it is just for tutorial purposes
+export const hasEnvVars =
+  process.env.NEXT_PUBLIC_SUPABASE_URL &&
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY;
