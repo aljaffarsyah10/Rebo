367e0ef8809df39d77826a0e2eb0d14636021baf
aljaffarsyah <aljaffarsyah10@gmail.com>
2025-08-31 02:57:41 +0800

    add tooltip for icon header

--- src/components/layout/cta-github.tsx
+++ src/components/layout/cta-github.tsx
@@ -1,18 +1,29 @@
+'use client';
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { IconBrandGithub } from '@tabler/icons-react';
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent
+} from '@/components/ui/tooltip';
 
 export default function CtaGithub() {
   return (
-    <Button variant='ghost' asChild size='sm' className='hidden sm:flex'>
-      <a
-        // href='https://github.com/aljaffarsyah10/Rebo'
-        rel='noopener noreferrer'
-        target='_blank'
-        className='dark:text-foreground'
-      >
-        <IconBrandGithub />
-      </a>
-    </Button>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant='ghost' asChild size='sm' className='hidden sm:flex'>
+          <a
+            // href='https://github.com/aljaffarsyah10/Rebo'
+            rel='noopener noreferrer'
+            target='_blank'
+            className='dark:text-foreground'
+          >
+            <IconBrandGithub />
+          </a>
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent sideOffset={6}>GitHub</TooltipContent>
+    </Tooltip>
   );
 }
--- src/components/layout/cta-panduan.tsx
+++ src/components/layout/cta-panduan.tsx
@@ -1,18 +1,30 @@
+'use client';
+
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/icons';
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent
+} from '@/components/ui/tooltip';
 
 export default function CtaPanduan() {
   return (
-    <Button variant='ghost' asChild size='sm' className='hidden sm:flex'>
-      <a
-        // href='https://youtu.be/8Q5mt4glAd4'
-        rel='noopener noreferrer'
-        target='_blank'
-        className='dark:text-foreground'
-      >
-        <Icons.video />
-      </a>
-    </Button>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant='ghost' asChild size='sm' className='hidden sm:flex'>
+          <a
+            href='https://youtu.be/8Q5mt4glAd4'
+            rel='noopener noreferrer'
+            target='_blank'
+            className='dark:text-foreground'
+          >
+            <Icons.video />
+          </a>
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent sideOffset={6}>video panduan</TooltipContent>
+    </Tooltip>
   );
 }
